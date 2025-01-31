55ade0f55f89097d1483f221d31120dbddaa0c78
joakim ireri <joeahkim@gmail.com>
2025-01-31 15:36:45 +0300

    Continue statements

---
+++ ControlStatements/Continue.java
@@ -0,0 +1,12 @@
+package ControlStatements;
+
+public class Continue {
+    public static void main(String[] args) {
+        for (int i = 65; i <= 122; i++) {
+            if (i > 90 && i < 97) {
+                continue;
+            }
+            System.out.print((char) i + " ");
+        }
+    }
+}
